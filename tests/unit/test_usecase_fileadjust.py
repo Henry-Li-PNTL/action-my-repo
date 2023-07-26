db7788b6d6318f1ff658a5d15321c5bfccab8ecf
Henry Li <henry.li@pentium.network>
2023-07-26 16:43:04 +0800

    test: add file adjust test

---
+++ tests/unit/test_usecase_fileadjust.py
@@ -0,0 +1,119 @@
+import pytest
+
+from src.application.usecase.file_adjust import FileAdjustUseCase
+from src.domain.model.action import UpdateHelmByMicroSvcModel
+
+CORRECT_RAW_CONTENTS = [
+    """
+- name: example-app
+  <<: *anchor
+  needs:
+  - example-main-app
+  chart: example/pare-example
+  version: 0.5.0
+  values:
+    - image: preview.example.com/example-app
+    - appVersion: 01-abcdef6
+    """,
+    """
+- name: example-app
+  values:
+    - appVersion: 01-abcdef6
+    """,
+    """
+- name: example-app
+  <<: *anchor
+  needs:
+  - example-main-app
+  chart: example/pare-example
+  version: 0.5.0
+  values:
+    - image: preview.example.com/example-app
+    - appVersion: 01-abcdef6
+    """,
+    """
+- name: example-app
+  values:
+    - image: preview.example.com/example-app
+    - appVersion: 01-abcdef6
+    - extraContainer:
+        image: preview.example.com/example-app
+        appVersion: 0.0.0
+        pullPolicy: Always
+        """,
+    """
+- name: example-app
+  values:
+    - image: preview.example.com/example-app
+    - appVersion: not_a_target
+    - extraContainer:
+        image: preview.example.com/example-app
+        appVersion: 0.0.0
+        pullPolicy: Always
+- name: example-app-2
+  values:
+    - image: preview.example.com/example-app
+    - appVersion: 01-abcdef6
+    - extraContainer:
+        image: preview.example.com/example-app
+        appVersion: 0.0.0
+        pullPolicy: Always
+- name: example-app-3
+  values:
+    - image: preview.example.com/example-app
+    - appVersion: not_a_target
+    - extraContainer:
+        image: preview.example.com/example-app
+        appVersion: 0.0.0
+        pullPolicy: Always""",
+]
+
+SPACE_PREFIX_RAW_CONTENTS = [
+    """
+    - name: example-app
+    values:
+        - image: preview.example.com/example-app
+        - appVersion: not_a_target
+        - extraContainer:
+            image: preview.example.com/example-app
+            appVersion: 0.0.0
+            pullPolicy: Always
+    """,
+    """
+    - name: example-app
+        values:
+            - image: preview.example.com/example-app
+            - appVersions: not_a_target
+        - extraContainer:
+            image: preview.example.com/example-app
+            appVersion: 0.0.0
+            pullPolicy: Always
+    """
+]
+
+
+@pytest.mark.parametrize(
+    "data,raw_content",
+    [
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app", target_app_version="20-example"), CORRECT_RAW_CONTENTS[0]),
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app", target_app_version="20-example"), CORRECT_RAW_CONTENTS[1]),
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app", target_app_version="20-example"), CORRECT_RAW_CONTENTS[2]),
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app", target_app_version="20-example"), CORRECT_RAW_CONTENTS[3]),
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app-2", target_app_version="20-example"), CORRECT_RAW_CONTENTS[4]),
+    ],
+)
+def test_update_helm_by_microsvc_model_with_different__appversion_position(data: UpdateHelmByMicroSvcModel, raw_content: str):
+    correct_answer = raw_content.replace("01-abcdef6", data.target_app_version, 1)
+    assert FileAdjustUseCase.replace_app_version(data=data, raw_content=raw_content) == correct_answer
+
+
+
+@pytest.mark.parametrize(
+    "data,raw_content",
+    [
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app", target_app_version="20-example"), SPACE_PREFIX_RAW_CONTENTS[0]),
+        (UpdateHelmByMicroSvcModel(base="", head="", target_repo="example-app", target_app_version="20-example"), SPACE_PREFIX_RAW_CONTENTS[1]),
+    ],
+)
+def test_update_helm_by_microsvc_model_with_space_prefix_name(data: UpdateHelmByMicroSvcModel, raw_content: str):
+    assert FileAdjustUseCase.replace_app_version(data=data, raw_content=raw_content) == raw_content
