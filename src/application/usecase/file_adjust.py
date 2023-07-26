969670c6be8f5858960eda5300c232c2c0e0baa1
Henry Li <henry.li@pentium.network>
2023-07-26 11:35:01 +0800

    feat: add file adjust usecase

---
+++ src/application/usecase/file_adjust.py
@@ -0,0 +1,36 @@
+import re
+
+from src.common.constants import FIND_SPECIFY_MICROSVC_RE
+from src.domain.model.action import UpdateHelmByMicroSvcModel
+
+
+class FileAdjustUseCase:
+    @staticmethod
+    def replace_app_version(data: UpdateHelmByMicroSvcModel, raw_content: str) -> str:
+        """replace app version in file
+
+        Args:
+            data (UpdateHelmByMicroSvcModel): update helm by micro svc model
+            raw_content (str): raw file content
+
+        Returns:
+            str: clean file content
+
+        Notes:
+            - use re.DOTALL to replace all line
+            - use re.sub to replace all app version in file
+            - use lambda to replace app version in file
+            - use groupdict to get app version group
+            - use group to get app version group
+            - use group.groupdict() to get app version group dict
+            - use group.group() to get app version group string
+
+        """
+
+        def group_adjust_fn(m: re.Match) -> str:
+            return str(m.group().replace(m.groupdict()["appVersion"], data.target_app_version))
+
+        pattern = FIND_SPECIFY_MICROSVC_RE.format(data.target_repo)
+        clean_file = re.sub(pattern, group_adjust_fn, raw_content, flags=re.DOTALL)
+
+        return clean_file
