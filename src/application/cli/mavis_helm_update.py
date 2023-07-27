5b808e6a0b06131cc74aabe047dc1151bcfa06ee
Henry Li <henry.li@pentium.network>
2023-07-27 11:04:21 +0800

    fix: instantiate GithubEnv in config.py

--- src/application/cli/mavis_helm_update.py
+++ src/application/cli/mavis_helm_update.py
@@ -2,8 +2,8 @@ from typer import Typer
 
 from src.application.usecase.github import GithubManager
 from src.domain.model.action import UpdateHelmByMicroSvcModel
-from src.domain.model.env import GithubEnv
 from src.infra.github import GithubRepository
+from src.application.config import github_env
 
 action_app = Typer()
 
@@ -12,10 +12,9 @@ action_app = Typer()
 def pull_request(base: str, head: str, target_repo: str, target_repo_app_version: str) -> None:
     """Send pull request to mavis"""
     # Gather data
-    secret = GithubEnv()  # type: ignore
     action_data = UpdateHelmByMicroSvcModel(base, head, target_repo, target_repo_app_version)
 
-    action_github_repo = GithubRepository(secret)
+    action_github_repo = GithubRepository(github_env)
 
     # Use Case
     manager = GithubManager(action_github_repo, action_data)
---
+++ src/application/config.py
@@ -0,0 +1,3 @@
+from src.domain.model.env import GithubEnv
+
+github_env = GithubEnv()  # type: ignore
