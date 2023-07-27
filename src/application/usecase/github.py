ee997296262c5cf16162abb2ffa29102465b9192
Henry Li <henry.li@pentium.network>
2023-07-27 15:36:49 +0800

    refactor: refactor usecase github code

--- src/application/usecase/github.py
+++ src/application/usecase/github.py
@@ -32,23 +32,29 @@ class GithubManager():
     def update_helm_and_pr(self) -> None:
 
         mavis_pr_base_branch_name = self.analyze_target_branch_name(self._mavis_repo, self.data.head)
-
-        # Get or create auto pr branch
         mavis_pr_head_branch_ref = self.get_or_create_auto_pr_branch(
             self._mavis_repo,
             branch_name=mavis_pr_base_branch_name
         )
 
-        # fetch content and update helm file and commit changes
-        self.update_helm_and_commit(ref=mavis_pr_head_branch_ref)
+        self.update_helm_and_commit(repo=self._mavis_repo,ref=mavis_pr_head_branch_ref)
 
-        # create pull request
-        self.create_pull_request(head=mavis_pr_head_branch_ref.ref, base=mavis_pr_base_branch_name)
+        self.create_pull_request(repo=self._mavis_repo, head=mavis_pr_head_branch_ref.ref, base=mavis_pr_base_branch_name)
 
-    def update_helm_and_commit(self, ref: GitRef.GitRef, file_path: str = "helmfile.yaml") -> None:
-        mavis_github_repo = self._get_mavis_repo()
+    def update_helm_and_commit(self, repo: Repository.Repository, ref: GitRef.GitRef, file_path: str = "helmfile.yaml") -> None:
+        """Fetch Helmfile, Update appVersion,
 
-        content_file = self.action_github_repo.get_file(file_path, ref, github_repo=mavis_github_repo)
+        Args:
+            repo (Repository.Repository): Github repo
+            ref (GitRef.GitRef): Branch we fetch file from
+            file_path (str, optional): file path. Defaults to "helmfile.yaml".
+
+        Raises:
+            MultipleFileFoundError: if multiple file found
+            NoContentError: if file is not found
+        """
+
+        content_file = self.action_github_repo.get_file(file_path, ref, github_repo=repo)
 
         if isinstance(content_file, list):
             raise MultipleFileFoundError(f"Multiple files found for {file_path} | ref={ref}")
@@ -63,9 +69,9 @@ class GithubManager():
 
         # Update helm file and commit changes
         # Commit file change to the commit sha we clone
-        mavis_github_repo.update_file(
+        repo.update_file(
             path=file_path,
-            message="update README.md",
+            message=f"chore: update helmfile {self.data.target_repo} appVersion to {self.data.target_app_version}",
             content=_clean_content,
             sha=content_file.sha,
             branch=ref.ref
@@ -76,6 +82,7 @@ class GithubManager():
         If exists, then reture that branch
 
         Args:
+            repo (Repository.Repository): target github repo
             branch_name (str): Target branch name
 
         Returns:
@@ -96,6 +103,11 @@ class GithubManager():
         """The base branch of a pull request can be either "master" or
         the same as the head branch name that triggered the GitHub Action pull request event.
 
+        Args:
+            repo (Repository.Repository): _description_
+            try_branch (str): return try_branch if exists.
+            default_branch (str, optional): if try_branch is not exists, then use this default_branch name. Defaults to MAVIS_MAIN_BRANCH.
+
         Returns:
             str: New Pull Request Base Branch Name
         """
@@ -109,6 +121,7 @@ class GithubManager():
 
     def create_pull_request(
         self,
+        repo: Repository.Repository,
         head: str,
         base: str,
         title: str | None = None,
@@ -118,23 +131,19 @@ class GithubManager():
         Create a pull request to specify branch.
         Merge head to base.
 
-        :param head: New Pull Request Base Branch Name. IMPORTANT: Don't need to add 'heads' prefix to branch name
-        :type branch_name: str
-        :param base: New Pull Request Base Branch Name. IMPORTANT: Don't need to add 'heads' prefix to Base name
-        :type base: str
-        :param title: New Pull Request Title
-        :type title: str
-        :param body: New Pull Request Body
-        :type body: str
+        Args:
+            repo (Repository.Repository): _description_
+            head (str): New Pull Request Base Branch Name. IMPORTANT: Don't need to add 'heads' prefix to branch name
+            base (str): New Pull Request Base Branch Name. IMPORTANT: Don't need to add 'heads' prefix to Base name
+            title (str | None, optional): New Pull Request Title. Defaults to None. If it's None, will give a proper default title.
+            body (str | None, optional): New Pull Request Body. Defaults to None. If it's None, will give a proper default body.
         """
 
         pr_title = title or "[Auto Pull Request] Update helmfile for micro service"
         f"'{self.data.target_repo}' appVersion to {self.data.target_app_version}"
         pr_body = body or f"""{pr_title}"""
 
-        mavis_repo = self._get_mavis_repo()
-
-        mavis_repo.create_pull(
+        repo.create_pull(
             title=pr_title,
             body=pr_body,
             base=base,
